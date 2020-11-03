f271255f5750ea76e579a8a945f7944400cbe1c1
Frank Samiec <frank.samiec@fritz.box>
2020-11-04 00:40:45 +0100

    Keypad functions as expected

--- src/ButtonBox.ino
+++ src/ButtonBox.ino
@@ -5,140 +5,47 @@
 #define NUMROTARIES 3
 #define NUMBUTTONS 18
 #define NUMHATS 1
-#define NUMROWS 3
-#define NUMCOLS 4
+#define NUMROWS 4
+#define NUMCOLS 3
 
 const int eBrakeButtonMap = A3;
-
 const int shifterUpButtonMap = 2;
 const int shifterDownButtonMap = 3;
 
-byte buttons[NUMROWS][NUMCOLS] = {
-  {0,1,2,3},
-  {4,5,6,7},
-  {8,9,10,11}  
-};
-
-struct rotariesdef {
-  byte pin1;
-  byte pin2;
-  int ccwchar;
-  int cwchar;
-  volatile unsigned char state;
-};
-
-//Arduino In, Arduino Out, Rotary Left, Rotary Right
-rotariesdef rotaries[NUMROTARIES] {
-  {4,5,12,13,0},
-  {6,7,14,15,0},
-  {8,9,16,17,0}  
-};
-
-#define DIR_CCW 0x10
-#define DIR_CW 0x20
-#define R_START 0x0
-
-#ifdef HALF_STEP
-#define R_CCW_BEGIN 0x1
-#define R_CW_BEGIN 0x2
-#define R_START_M 0x3
-#define R_CW_BEGIN_M 0x4
-#define R_CCW_BEGIN_M 0x5
-const unsigned char ttable[6][4] = {
-  // R_START (00)
-  {R_START_M,            R_CW_BEGIN,     R_CCW_BEGIN,  R_START},
-  // R_CCW_BEGIN
-  {R_START_M | DIR_CCW, R_START,        R_CCW_BEGIN,  R_START},
-  // R_CW_BEGIN
-  {R_START_M | DIR_CW,  R_CW_BEGIN,     R_START,      R_START},
-  // R_START_M (11)
-  {R_START_M,            R_CCW_BEGIN_M,  R_CW_BEGIN_M, R_START},
-  // R_CW_BEGIN_M
-  {R_START_M,            R_START_M,      R_CW_BEGIN_M, R_START | DIR_CW},
-  // R_CCW_BEGIN_M
-  {R_START_M,            R_CCW_BEGIN_M,  R_START_M,    R_START | DIR_CCW},
-};
-#else
-#define R_CW_FINAL 0x1
-#define R_CW_BEGIN 0x2
-#define R_CW_NEXT 0x3
-#define R_CCW_BEGIN 0x4
-#define R_CCW_FINAL 0x5
-#define R_CCW_NEXT 0x6
-
-const unsigned char ttable[7][4] = {
-  // R_START
-  {R_START,    R_CW_BEGIN,  R_CCW_BEGIN, R_START},
-  // R_CW_FINAL
-  {R_CW_NEXT,  R_START,     R_CW_FINAL,  R_START | DIR_CW},
-  // R_CW_BEGIN
-  {R_CW_NEXT,  R_CW_BEGIN,  R_START,     R_START},
-  // R_CW_NEXT
-  {R_CW_NEXT,  R_CW_BEGIN,  R_CW_FINAL,  R_START},
-  // R_CCW_BEGIN
-  {R_CCW_NEXT, R_START,     R_CCW_BEGIN, R_START},
-  // R_CCW_FINAL
-  {R_CCW_NEXT, R_CCW_FINAL, R_START,     R_START | DIR_CCW},
-  // R_CCW_NEXT
-  {R_CCW_NEXT, R_CCW_FINAL, R_CCW_BEGIN, R_START},
-};
-#endif
-
-byte rowPins[NUMROWS] = {20,19,18,15}; 
+byte buttons[NUMROWS][NUMCOLS] = {{0,1,2},{3,4,5},{6,7,8},{9,10,11}};
+byte rowPins[NUMROWS] = {A2,A1,A0,15}; 
 byte colPins[NUMCOLS] = {14,16,10}; 
+Keypad keypad = Keypad(makeKeymap(buttons), rowPins, colPins, NUMROWS, NUMCOLS); 
 
-Keypad buttbx = Keypad( makeKeymap(buttons), rowPins, colPins, NUMROWS, NUMCOLS); 
 Joystick_ Joystick(
   JOYSTICK_DEFAULT_REPORT_ID,
-  JOYSTICK_TYPE_JOYSTICK,
+  JOYSTICK_TYPE_GAMEPAD,
   NUMBUTTONS, NUMHATS,    // Button Count, Hat Switch Count
   false, false, false,    // X, Y, Z Axis
   false, false, false,    // Rx, Ry, Rz
   false, false,           // rudder or throttle
   false, true, false);    // accelerator, brake, or steering
 
-
-
 void setup() {
   pinMode(shifterUpButtonMap, INPUT_PULLUP);
   pinMode(shifterDownButtonMap, INPUT_PULLUP);
   
-  Joystick.begin();
-  setup_rotaries();
+  Joystick.begin();  
 }
-
-void setup_rotaries() {
-  for (int i=0;i<NUMROTARIES;i++) {
-    pinMode(rotaries[i].pin1, INPUT);
-    pinMode(rotaries[i].pin2, INPUT);
-    #ifdef ENABLE_PULLUPS
-      digitalWrite(rotaries[i].pin1, HIGH);
-      digitalWrite(rotaries[i].pin2, HIGH);
-    #endif
-  }
-}
-
 
 void loop() {
- 
-  readEBrake();  
-  readShifter();
-
-  CheckAllEncoders();
-  CheckAllButtons();
-
-  delay(50);
+  ReadEBrake();  
+  ReadShifter();
+  ReadButtons();
 }
 
-void readEBrake()
-{
+void ReadEBrake() {
   int pot = analogRead(eBrakeButtonMap);
   int mapped = map(pot,0,1023,0,255);
   Joystick.setBrake(mapped);
 }
 
-void readShifter()
-{   
+void ReadShifter() {   
   int upShift = !digitalRead(shifterUpButtonMap);
   int downShift = !digitalRead(shifterDownButtonMap);
 
@@ -151,43 +58,22 @@ void readShifter()
   }
 }
 
-void CheckAllButtons(void) {
-      if (buttbx.getKeys())
-    {
-       for (int i=0; i<LIST_MAX; i++)   
-        {
-           if ( buttbx.key[i].stateChanged )   
-            {
-            switch (buttbx.key[i].kstate) {  
-                    case PRESSED:
-                    case HOLD:
-                              Joystick.setButton(buttbx.key[i].kchar, 1);
-                              break;
-                    case RELEASED:
-                    case IDLE:
-                              Joystick.setButton(buttbx.key[i].kchar, 0);
-                              break;
-            }
-           }   
-         }
-     }
-}
-
-unsigned char rotary_process(int _i) {
-   unsigned char pinstate = (digitalRead(rotaries[_i].pin2) << 1) | digitalRead(rotaries[_i].pin1);
-  rotaries[_i].state = ttable[rotaries[_i].state & 0xf][pinstate];
-  return (rotaries[_i].state & 0x30);
-}
-
-
-void CheckAllEncoders(void) {
-  for (int i=0;i<NUMROTARIES;i++) {
-    unsigned char result = rotary_process(i);
-    if (result == DIR_CCW) {
-      Joystick.setButton(rotaries[i].ccwchar, 1); delay(50); Joystick.setButton(rotaries[i].ccwchar, 0);
-    };
-    if (result == DIR_CW) {
-      Joystick.setButton(rotaries[i].cwchar, 1); delay(50); Joystick.setButton(rotaries[i].cwchar, 0);
-    };
+void ReadButtons() {
+  if (keypad.getKeys()){
+    for (int i=0; i<LIST_MAX; i++) {
+      if (keypad.key[i].stateChanged) {
+        switch (keypad.key[i].kstate) {  
+          case PRESSED:
+          case HOLD:
+            Joystick.setButton(keypad.key[i].kchar, 1);
+            break;
+          case RELEASED:
+          case IDLE:
+            Joystick.setButton(keypad.key[i].kchar, 0);
+            break;
+        }
+      }   
+    }
   }
 }
+
